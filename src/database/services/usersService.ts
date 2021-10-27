28370beff0acbc652c372dccdf256fab242f6616
Swackles <Swackles@users.noreply.github.com>
2021-10-27 18:53:05 +0300

    HV-14 type User renamed to UserDatabaseType

--- src/database/services/usersService.ts
+++ src/database/services/usersService.ts
@@ -1,8 +1,8 @@
-import User from '../types/User'
+import UserDatabaseType from '../types/UserDatabaseType'
 import {query} from './databaseService'
 
-export async function findUserWithEmail(email: string): Promise<User | null> {
-	const res = await query<User>('SELECT * FROM users WHERE email = $1', [email])
+export async function findUserWithEmail(email: string): Promise<UserDatabaseType | null> {
+	const res = await query<UserDatabaseType>('SELECT * FROM users WHERE email = $1', [email])
 
 	return res.rows[0] || null
 }
--- src/database/types/UserDatabaseType.ts
+++ src/database/types/UserDatabaseType.ts
@@ -1,9 +1,9 @@
 import BaseType from './BaseType'
 
-type User = BaseType & {
+type UserDatabaseType = BaseType & {
 	name: string
 	email: string
 	password: string
 }
 
-export default User
+export default UserDatabaseType
