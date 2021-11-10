20ea4a302ca63cc42e716f4e227a7769a8d0571b
Swackles <Swackles@users.noreply.github.com>
2021-11-10 22:42:42 +0200

    HV-32 added file for finding file in database

--- src/database/services/filesDatabaseService.ts
+++ src/database/services/filesDatabaseService.ts
@@ -4,6 +4,15 @@ import FileDatabaseType from '../types/FileDatabaseType'
 import SaveFileType from '../types/SaveFileType'
 import fileMapper from '../mappers/fileMapper'
 
+async function findWithNameAndExtension(name: string, extension: string): Promise<File> {
+	const res = await query<FileDatabaseType>(
+		'SELECT * FROM files WHERE extension = $1 AND name = $2',
+		[extension, name]
+	)
+
+	return fileMapper(res.rows[0])
+}
+
 async function save(file: SaveFileType): Promise<File> {
 	const res = await query<FileDatabaseType>(
 		'INSERT INTO files (name, extension, original_name) VALUES ($1, $2, $3) RETURNING *',
@@ -13,5 +22,6 @@ async function save(file: SaveFileType): Promise<File> {
 }
 
 export default {
+	findWithNameAndExtension,
 	save
 }
--- test/database/services/filesDatabaseService.test.ts
+++ test/database/services/filesDatabaseService.test.ts
@@ -1,5 +1,37 @@
 import { query } from '../../../src/database/services/databaseService'
 import filesDatabaseService from '../../../src/database/services/filesDatabaseService'
+import faker from 'faker'
+import FileDatabaseType from '../../../src/database/types/FileDatabaseType'
+
+describe('findWithNameAndExtension', () => {
+	const originalName = faker.random.word()
+	const data = [`${faker.datatype.uuid()}-${originalName}`, 'pdf', originalName]
+	let id: number
+
+	beforeEach(async () => {
+		const res = await query<FileDatabaseType>('INSERT INTO files (name, extension, original_name) VALUES ($1, $2, $3) RETURNING *', data)
+		id = res.rows[0].id
+	})
+
+	afterEach(async () => {
+		await query('DELETE FROM files WHERE id = $1', [id])
+	})
+
+	it('should return a file', async () => {
+		const res = await filesDatabaseService.findWithNameAndExtension(data[0], data[1])
+
+		expect(res.id).toBe(id)
+		expect(res.name).toBe(data[0])
+		expect(res.extension).toBe(data[1])
+		expect(res.originalName).toBe(data[2])
+	})
+
+	it('should return null', async () => {
+		const res = await  filesDatabaseService.findWithNameAndExtension('no exists', 'test')
+
+		expect(res).toBeNull()
+	})
+})
 
 describe('save', () => {
 	const uniqueKey = 'FILES_DATABASE_SERVICE_SAVE_'
